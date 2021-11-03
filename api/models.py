a42997143e27c8aa97ea5830c0addbf5d7255141
brodie-m <brodiedmcguire@gmail.com>
2021-11-03 11:58:58 +0000

    all priceS

--- api/models.py
+++ api/models.py
@@ -1,19 +1,37 @@
+from time import sleep
 from django.db import models
+import matplotlib.pyplot as plt
+import numpy as np
 import random
-from pricing import price
+
 
 # Create your models here.
+
+
 class ETF(models.Model):
     ticker = models.CharField(max_length=100)
-    current_price = models.DecimalField(max_digits=2)
-    previous_price = models.DecimalField(max_digits=2)
-    sigma = models.IntegerField()
+    current_price = models.DecimalField(decimal_places=3, max_digits=5)
+    previous_price = models.DecimalField(decimal_places=3, max_digits=5)
+    sigma = models.DecimalField(decimal_places=3, max_digits=5)
+    all_prices = models.CharField(max_length=100000)
     
 
+    def price(self, initial_price, timestep, count):
+        if initial_price <= 0:
+            return
+        if count < 1:
+            return
+        difference = (-1)**np.round(np.random.rand())*(np.random.rand() + np.random.rand()*timestep**(0.5))
+        newPrice = initial_price + difference
+        return newPrice
+    
     def update_price(self):
+        self.all_prices += f'{self.current_price},'
         self.previous_price = self.current_price
-        new_price = price(self.current_price,self.sigma,1,1)
+        new_price = self.price(self.current_price, 1, 1)
         self.current_price = new_price
 
-        
-print(type(ETF))
+prices=[]
+days=[]
+etf_1=ETF('facebook',5,5,0.1,'')
+
