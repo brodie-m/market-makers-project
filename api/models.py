ba00e3db31ea82af351fcfd7b1c7458f4bd45be6
brodie-m <brodiedmcguire@gmail.com>
2021-11-03 10:54:28 +0000

    changes to update_price

--- api/models.py
+++ api/models.py
@@ -4,13 +4,14 @@ from pricing import price
 
 # Create your models here.
 class ETF(models.Model):
+    ticker = models.CharField(max_length=100)
     current_price = models.DecimalField(max_digits=2)
     previous_price = models.DecimalField(max_digits=2)
     sigma = models.IntegerField()
     
 
-    def update_price(self,current_price,previous_price):
-        previous_price = self.current_price
+    def update_price(self):
+        self.previous_price = self.current_price
         new_price = price(self.current_price,self.sigma,1,1)
         self.current_price = new_price
 
